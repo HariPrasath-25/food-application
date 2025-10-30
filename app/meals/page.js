a191117d26de434173f83a8f973dbaf79bfd22a0
Hari Prasath <prasathhari256@gmail.com>
2025-10-30 17:25:42 +0530

    Add pages for meals and community

---
+++ app/meals/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+    return (
+        <>
+            <h1>Meals Page</h1>
+            <p><Link href="/meals/sharemeal">Meals Share</Link></p>
+        </>
+    )
+}
